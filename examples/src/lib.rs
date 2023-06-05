c137e655524aebfad7017456cd1d35eb578ea007
Just van Stam <just.van.stam@gmail.com>
2023-06-05 09:03:42 +0200

    different structure in example folders

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -1,12 +1,23 @@
-mod expects;
-mod fees;
-mod first_look;
-mod holding_modifiers;
-mod locks;
-mod origins;
-mod queries;
 mod simple_test_net;
-mod transact;
+#[path ="0_first_look/mod.rs"]
+mod first_look;
+#[path="1_transfers/mod.rs"]
 mod transfers;
+#[path ="2_fees/mod.rs"]
+mod fees;
+#[path ="3_transact/mod.rs"]
+mod transact;
+#[path ="4_origins/mod.rs"]
+mod origins;
+#[path ="5_holding_modifiers/mod.rs"]
+mod holding_modifiers;
+#[path ="6_trap_and_claim/mod.rs"]
 mod trap_and_claim;
+#[path="7_expects/mod.rs"]
+mod expects;
+#[path ="8_queries/mod.rs"]
+mod queries;
+#[path ="9_version_subscription/mod.rs"]
 mod version_subscription;
+#[path ="10_locks/mod.rs"]
+mod locks;
